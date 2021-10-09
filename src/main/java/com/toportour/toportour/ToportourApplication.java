56234da8c5a56c9564d472ecccff653cc8945d2b
Archibald5 <artur.topor@mail.ru>
2021-10-09 20:02:37 +0300

    first commit

---
+++ src/main/java/com/toportour/toportour/ToportourApplication.java
@@ -0,0 +1,13 @@
+package com.toportour.toportour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToportourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToportourApplication.class, args);
+	}
+
+}
